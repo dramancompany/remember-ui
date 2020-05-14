49a899ba085a29080dc48acc6e1679229b14608c
Jisuk Yoon <thearclien@gmail.com>
2020-05-14 15:56:21 +0900

    Docz color 추가

---
+++ docsSrc/components/Remember/colorItem.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  border-radius: 4px;
+  margin: 4px 0;
+  padding: 6px 12px;
+  background-color: ${props => props.backgroundColor};
+  color: ${props => props.color || '#fff'};
+  cursor: pointer;
+  transition: all 0.1s cubic-bezier(0.4, 1, 0.75, 0.9);
+  border: 1px solid rgba(16, 22, 26, 0.2);
+  &::after {
+    content: "${props => props.content}";
+  }
+  &:hover {
+    border: 1px solid rgba(16, 22, 26, 0.2);
+    box-shadow: inset 0 0 0 1px rgba(16, 22, 26, 0.2), 0 2px 4px rgba(16, 22, 26, 0.1), 0 2px 4px rgba(16, 22, 26, 0.2);
+    &::after {
+      content: "Click to copy hex code";
+    }
+  }
+`;
+
+const HexCode = styled.span`
+  float: right;
+`;
+
+const handleClickItem = backgroundColor => {
+  const element = document.createElement('textarea');
+  element.value = backgroundColor.toUpperCase();
+  element.setAttribute('readonly', '');
+  element.style.position = 'absolute';
+  element.style.left = '-9999px';
+  document.body.appendChild(element);
+  element.select();
+  document.execCommand('copy');
+  document.body.removeChild(element);
+};
+
+export const ColorItem = ({ backgroundColor, colorName, color, ...rest }) => (
+  <Container
+    color={color}
+    backgroundColor={backgroundColor}
+    content={colorName}
+    onClick={() => handleClickItem(backgroundColor)}
+    {...rest}
+  >
+    <HexCode>{backgroundColor.toUpperCase()}</HexCode>
+  </Container>
+);
