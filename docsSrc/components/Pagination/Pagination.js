3b10dc421c50933d6a85a272270b5d34fe940326
Jisuk Yoon <thearclien@gmail.com>
2020-05-14 10:59:19 +0900

    Docz pagination 추가

---
+++ docsSrc/components/Pagination/Pagination.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import t from 'prop-types';
+import { DcNewPagination as Component } from 'remember-ui';
+
+export const Pagination = ({ ...rest }) => <Component {...rest} />;
+
+Pagination.propTypes = {
+  activePage: t.number,
+  totalItemCount: t.number,
+  pageRangeDisplayed: t.number,
+  itemCountPerPage: t.number,
+  onChangePage: t.func,
+};
+
+Pagination.defaultProps = {
+  activePage: 1,
+  totalItemCount: 0,
+  onChangePage: () => {},
+};
