2f8e0b7ed1bc14f8e852b5647db12322880228de
myoungho.pak <qkraudghgh@gmail.com>
2020-06-26 12:41:49 +0900

    profile avatar svg 크기 IE 대응

--- src/components/Icon/ProfileAvatar/ProfileAvatar.styles.js
+++ src/components/Icon/ProfileAvatar/ProfileAvatar.styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: ${({ width }) => `${width}px`};
+  height: ${({ width }) => `${width}px`};
 `;
 
 export const Svg = styled.svg`
