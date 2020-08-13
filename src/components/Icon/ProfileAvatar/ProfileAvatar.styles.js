6f69c9c5dda2d9cfae104307f61e86d6c2105cd2
jiseop.han <jiseop.han@dramancompany.com>
2020-08-13 15:18:21 +0900

    PCWEB-4440 add border to profile avatar

--- src/components/Icon/ProfileAvatar/ProfileAvatar.styles.js
+++ src/components/Icon/ProfileAvatar/ProfileAvatar.styles.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: ${({ width }) => `${width}px`};
   height: ${({ width }) => `${width}px`};
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  border-radius: 50%;
 `;
 
 export const Svg = styled.svg`
