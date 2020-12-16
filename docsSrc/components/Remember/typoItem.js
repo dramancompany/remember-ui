5a77cfcd908f1e25c008003a0c1390df9e527f5f
Jisuk Yoon <thearclien@gmail.com>
2020-12-16 09:01:12 +0900

    PCWEB-5113 모바일 폰트 추가

--- docsSrc/components/Remember/typoItem.js
+++ docsSrc/components/Remember/typoItem.js
@@ -11,47 +11,108 @@ import {
   textExtraLarge,
   textDoubleExtraLarge,
   textTripleExtraLarge,
+  mobileTitle,
+  mobileSubTitle16,
+  mobileSubTitle14,
+  mobileSubTitle13,
+  mobileSubTitle12,
+  mobileBody15,
+  mobileBody13,
+  mobileBody12,
+  mobileSubCaption,
 } from 'remember-ui';
 
 const Container = styled.div`
-  margin: 4px 0;
+  margin: 10px 0;
   ${({ typo }) =>
     (typo === 'textExtraSmall' &&
       css`
-        ${textExtraSmall({ color: text })}
+        ${textExtraSmall({ color: text })};
       `) ||
     (typo === 'textSmall' &&
       css`
-        ${textSmall({ color: text })}
+        ${textSmall({ color: text })};
       `) ||
     (typo === 'textMedium' &&
       css`
-        ${textMedium({ color: text })}
+        ${textMedium({ color: text })};
       `) ||
     (typo === 'textLarge' &&
       css`
-        ${textLarge({ color: text })}
+        ${textLarge({ color: text })};
       `) ||
     (typo === 'textExtraLarge' &&
       css`
-        ${textExtraLarge({ color: text })}
+        ${textExtraLarge({ color: text })};
       `) ||
     (typo === 'textDoubleExtraLarge' &&
       css`
-        ${textDoubleExtraLarge({ color: text })}
+        ${textDoubleExtraLarge({ color: text })};
       `) ||
     (typo === 'textTripleExtraLarge' &&
       css`
-        ${textTripleExtraLarge({ color: text })}
+        ${textTripleExtraLarge({ color: text })};
+      `) ||
+    (typo === 'mobileTitle' &&
+      css`
+        ${mobileTitle({ color: text })};
+      `) ||
+    (typo === 'mobileSubTitle16' &&
+      css`
+        ${mobileSubTitle16({ color: text })};
+      `) ||
+    (typo === 'mobileSubTitle14' &&
+      css`
+        ${mobileSubTitle14({ color: text })};
+      `) ||
+    (typo === 'mobileSubTitle13' &&
+      css`
+        ${mobileSubTitle13({ color: text })};
+      `) ||
+    (typo === 'mobileSubTitle12' &&
+      css`
+        ${mobileSubTitle12({ color: text })};
+      `) ||
+    (typo === 'mobileBody15' &&
+      css`
+        ${mobileBody15({ color: text })};
+      `) ||
+    (typo === 'mobileBody13' &&
+      css`
+        ${mobileBody13({ color: text })};
+      `) ||
+    (typo === 'mobileBody12' &&
+      css`
+        ${mobileBody12({ color: text })};
+      `) ||
+    (typo === 'mobileSubCaption' &&
+      css`
+        ${mobileSubCaption({ color: text })};
       `) ||
     (typo === 'font' &&
       css`
-        ${font({ color: text })}
-      `)};
+        ${font({ color: text })};
+      `)}
+`;
+
+export const Title = styled.div`
+  ${textTripleExtraLarge({ color: text })};
+
+  margin: 0 0 20px;
+  padding: 10px 0px;
+  border-bottom: 1px solid ${text};
 `;
 
 export const TypeItem = ({ typo, ...rest }) => (
   <Container typo={typo} {...rest}>
     {typo}
+  </Container>
+);
+
+export const TypeMobileItem = ({ typo, children, ...rest }) => (
+  <Container typo={typo} {...rest}>
+    {typo}
+    <br />
+    {children}
   </Container>
 );
--- src/core/GlobalStyle/index.js
+++ src/core/GlobalStyle/index.js
@@ -1,3 +1,4 @@
 export * from './mixin';
+export * from './mixinMobile';
 export * from './variable';
 export * from './external';
