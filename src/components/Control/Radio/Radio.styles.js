c235c9afc5b425bda5ad47927c79d0acb91d8623
myoungho.pak <qkraudghgh@gmail.com>
2021-10-12 11:12:03 +0900

    PCWEB-6453 radio black icon fix

--- src/components/Control/Radio/Radio.styles.js
+++ src/components/Control/Radio/Radio.styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const RadioButton = styled.img`
-  height: ${({ theme }) => (theme === 'yellow' ? '18px' : '20px')};
-  width: ${({ theme }) => (theme === 'yellow' ? '18px' : '20px')};
+  height: ${({ theme }) => (theme === 'yellow' ? '18px' : '24px')};
+  width: ${({ theme }) => (theme === 'yellow' ? '18px' : '24px')};
   cursor: pointer;
 `;
