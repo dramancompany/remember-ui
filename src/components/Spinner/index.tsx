058ab0fd23c791f938879172a56e372b7056d0bd
saengmotmi <ohjtack@gmail.com>
2022-05-25 17:42:30 +0900

    PCWEB-7664 type: Spinner 컴포넌트

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 
 import { Container } from './Spinner.styles';
 
+export interface Props {
+  width?: number;
+  height?: number;
+  borderWeight?: number;
+  theme?: 'white' | 'gray';
+  className?: string;
+}
+
 export const Spinner = ({
   width = 32,
   height = 32,
   borderWeight = 5,
   theme = 'gray',
   className,
-}) => (
+}: Props) => (
   <Container
     className={className}
     width={width}
