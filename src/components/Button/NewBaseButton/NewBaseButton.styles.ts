bcc22f5e4d9c0d11dc1c77e84687e6c7e07e797a
kimjeongwon <kimjeongwonn@gmail.com>
2022-06-13 14:54:53 +0900

    PCWEB-7818 NewBaseButton.styles 타이핑

--- src/components/Button/NewBaseButton/NewBaseButton.styles.ts
+++ src/components/Button/NewBaseButton/NewBaseButton.styles.ts
@@ -1,49 +1,63 @@
 import styled, { css } from 'styled-components';
-import { disable, flexCenter, contents999 } from '../../../core/GlobalStyle';
+import { contents999, disable, flexCenter } from '../../../core/GlobalStyle';
 import { THEME_COLOR } from '../../../core/GlobalStyle/theme';
+import type { NewBaseButtonStyleType } from './NewBaseButton.types';
+
+const NEW_BASE_BUTTON_SIZES = {
+  small: {
+    fontSize: '12px',
+    height: '32px',
+    minWidth: '68px',
+    padding: '0 16px',
+  },
+  medium: {
+    fontSize: '14px',
+    height: '36px',
+    minWidth: '78px',
+    padding: '0 20px',
+  },
+  large: {
+    fontSize: '16px',
+    height: '44px',
+    minWidth: '108px',
+    padding: '0 20px',
+  },
+  xlarge: {
+    fontSize: '16px',
+    height: '52px',
+    minWidth: '116px',
+    padding: '0 24px',
+  },
+} as const;
 
 export const Inner = styled.div`
   display: flex;
   align-items: center;
 `;
 
-export const Container = styled.div`
+export const Container = styled.div<NewBaseButtonStyleType>`
   ${flexCenter};
 
   box-sizing: border-box;
   cursor: ${({ isLoading, disabled }) =>
     isLoading || disabled ? 'default' : 'pointer'};
 
-  height: ${({ size }) =>
-    (size === 'small' && '32px') ||
-    (size === 'medium' && '36px') ||
-    (size === 'large' && '44px') ||
-    (size === 'xlarge' && '52px')};
-
-  font-size: ${({ size }) =>
-    (size === 'small' && '12px') ||
-    (size === 'medium' && '14px') ||
-    (size === 'large' && '16px') ||
-    (size === 'xlarge' && '16px')};
-
-  min-width: ${({ size }) =>
-    (size === 'small' && '68px') ||
-    (size === 'medium' && '78px') ||
-    (size === 'large' && '108px') ||
-    (size === 'xlarge' && '116px')};
-
-  padding: ${({ size }) =>
-    (size === 'small' && '0 16px') ||
-    (size === 'medium' && '0 20px') ||
-    (size === 'large' && '0 20px') ||
-    (size === 'xlarge' && '0 24px')};
-
+  ${({ size = 'small' }) => css`
+    height: ${NEW_BASE_BUTTON_SIZES[size].height};
+    font-size: ${NEW_BASE_BUTTON_SIZES[size].fontSize};
+    min-width: ${NEW_BASE_BUTTON_SIZES[size].minWidth};
+    padding: ${NEW_BASE_BUTTON_SIZES[size].padding};
+    border-radius: ${size === 'small' ? '2px' : '4px'};
+  `};
   font-weight: 400;
   border-radius: 4px;
 
-  border: 1px solid ${({ theme }) => THEME_COLOR[theme].sub};
-  background-color: ${({ theme }) => THEME_COLOR[theme].sub};
   color: ${contents999};
+
+  ${({ buttonTheme }) => css`
+    border: 1px solid ${THEME_COLOR[buttonTheme].sub};
+    background-color: ${THEME_COLOR[buttonTheme].sub};
+  `};
 
   ${({ block }) =>
     block &&
@@ -51,27 +65,24 @@ export const Container = styled.div`
       width: 100%;
     `}
 
-  ${({ outline }) =>
+  ${({ outline, buttonTheme, disabled }) => [
     outline &&
-    css`
-      background-color: ${contents999};
-      color: ${({ theme }) => THEME_COLOR[theme].main};
-    `}
-
-  ${({ disabled }) =>
+      css`
+        background-color: ${contents999};
+        color: ${THEME_COLOR[buttonTheme].main};
+      `,
     disabled &&
-    css`
-      color: ${contents999};
-      border: 1px solid ${disable};
-      background-color: ${disable};
-    `};
-
-  ${({ disabled, outline }) =>
+      css`
+        color: ${contents999};
+        border: 1px solid ${disable};
+        background-color: ${disable};
+      `,
     disabled &&
-    outline &&
-    css`
-      border-color: ${disable};
-      color: ${disable};
-      background-color: ${contents999};
-    `}
+      outline &&
+      css`
+        border-color: ${disable};
+        color: ${disable};
+        background-color: ${contents999};
+      `,
+  ]}
 `;
