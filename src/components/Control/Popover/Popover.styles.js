021b1f52ddb2db9f4c018fcc5901d36e26cc8634
k.jeong <kwanwoo.jeong@gmail.com>
2022-07-11 12:08:20 +0900

    PCWEB-7987 Popover의 size prop option 추가

--- src/components/Control/Popover/Popover.styles.js
+++ src/components/Control/Popover/Popover.styles.js
@@ -1,12 +1,17 @@
 import styled, { css } from 'styled-components';
 import Tippy from '@tippy.js/react';
 
+const SIZE_TO_WIDTH_MAP = {
+  small: '170px',
+  large: '236px',
+  auto: 'auto',
+};
+
 export const Popover = styled(Tippy)`
   border-radius: 10px !important;
   background-color: white !important;
   box-shadow: 0 0px 16px 0 rgba(0, 0, 0, 0.2) !important;
-  width: ${({ $size = 'small' }) =>
-    $size === 'small' ? '170px' : '236px'} !important;
+  width: ${({ $size = 'small' }) => SIZE_TO_WIDTH_MAP[$size]} !important;
 
   .tippy-content {
     padding: 0 !important;
