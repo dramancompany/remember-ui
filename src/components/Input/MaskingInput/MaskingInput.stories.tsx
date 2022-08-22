fff4e13d327e469a13b9689fb7eb696f3d4311b4
JeongminSung <hellojeongmin@gmail.com>
2022-08-22 14:46:18 +0900

    PCWEB-8335 Textarea

--- src/components/Input/MaskingInput/MaskingInput.stories.tsx
+++ src/components/Input/MaskingInput/MaskingInput.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { MaskingInput } from '../index';
+import { MaskingInput } from './index';
 
 const meta: ComponentMeta<typeof MaskingInput> = {
   title: 'Input/MaskingInput',
