fdd42adec34961b38a1facef4d909f9ba8dd1607
JeongminSung <hellojeongmin@gmail.com>
2022-08-22 17:43:05 +0900

    PCWEB-8335 Canvas 백그라운드 컬러 옵션 추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,19 @@
 import React from 'react';
-// global decorators를 설정하기 위해 반드시 필요한 모듈
+// global decorators를 설정하기 위해 반드시 필요한 react 모듈 import
 
 /**
  * storybook layout settings
  */
 export const parameters = {
   layout: 'padded',
+  backgrounds: {
+    default: 'light',
+    values: [
+      { name: 'light', value: '#F8F8F8' },
+      { name: 'dark', value: '#333333' },
+      { name: 'white', value: '#fff' },
+    ],
+  },
   actions: { argTypesRegex: '^on[A-Z].*' },
   options: {
     showPanel: true,
