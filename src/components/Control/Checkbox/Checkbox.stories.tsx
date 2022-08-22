1b0781d6bc21f75523af231ac3480f6cd0c5ac82
JeongminSung <hellojeongmin@gmail.com>
2022-08-22 10:46:28 +0900

    PCWEB-8335 Checkbox

---
+++ src/components/Control/Checkbox/Checkbox.stories.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Checkbox } from './index';
+import { DoczContainer } from 'components/DoczContainer';
+
+const meta: ComponentMeta<typeof Checkbox> = {
+  title: 'Control/Checkbox',
+  component: Checkbox,
+};
+
+export const Variants: ComponentStory<typeof Checkbox> = (args) => {
+  return (
+    <>
+      <DoczContainer direction="column">
+        <h3>체크박스 on/off/disabled</h3>
+        <DoczContainer direction="row">
+          <Checkbox state="on" />
+          <Checkbox state="off" />
+          <Checkbox state="on" disabled />
+        </DoczContainer>
+      </DoczContainer>
+      <DoczContainer direction="column">
+        <h3>Round 체크박스 on/off</h3>
+        <DoczContainer direction="row">
+          <Checkbox round state="on" />
+          <Checkbox round state="off" />
+        </DoczContainer>
+      </DoczContainer>
+    </>
+  );
+};
+
+const Template: ComponentStory<typeof Checkbox> = (args) => (
+  <Checkbox {...args} />
+);
+
+export const Primary = Template.bind({});
+
+export default meta;
