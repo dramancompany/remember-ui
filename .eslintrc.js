209483bda7db376caabe226537c7e3086cf9a29d
JeongminSung <hellojeongmin@gmail.com>
2022-09-08 12:10:38 +0900

    PCWEB-8414 eslint prettier 세팅

---
+++ .eslintrc.js
@@ -0,0 +1,114 @@
+const path = require('path');
+
+module.exports = {
+  extends: ['react-app', 'airbnb-typescript', 'airbnb/hooks', 'prettier'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    // react
+    'react/jsx-filename-extension': 0,
+    'react/prop-types': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'react/destructuring-assignment': 0,
+    'react/no-unused-state': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/jsx-curly-newline': 0,
+    'react/jsx-wrap-multilines': 0,
+    'react/require-default-props': 0,
+    'react/no-unescaped-entities': 0,
+    'react/forbid-prop-types': 0,
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    // 웹 접근성
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
+    'jsx-a11y/no-autofocus': 0,
+    'jsx-a11y/mouse-events-have-key-events': 0,
+    'jsx-a11y/label-has-associated-control': 0,
+    'jsx-a11y/media-has-caption': 0,
+    // Javascript
+    'arrow-body-style': ['error', 'as-needed'],
+    'consistent-return': 0,
+    'no-underscore-dangle': 0,
+    'no-unused-expressions': 0,
+    'no-async-promise-executor': 0,
+    'no-restricted-syntax': 0,
+    'no-await-in-loop': 0,
+    'prefer-destructuring': ['error', { object: true, array: false }],
+    // Module
+    'import/no-extraneous-dependencies': 0,
+    'import/prefer-default-export': 0,
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['builtin', 'external'],
+          ['internal', 'parent', 'sibling'],
+        ],
+        pathGroups: [
+          {
+            pattern: 'global-style/**',
+            group: 'sibling',
+            position: 'after',
+          },
+          {
+            pattern: './*.constant',
+            group: 'sibling',
+            position: 'after',
+          },
+          {
+            pattern: './*.text',
+            group: 'sibling',
+            position: 'after',
+          },
+          {
+            pattern: './*.styles',
+            group: 'sibling',
+            position: 'after',
+          },
+          {
+            pattern: './*.scss',
+            group: 'sibling',
+            position: 'after',
+          },
+          {
+            pattern: './*.css',
+            group: 'sibling',
+            position: 'after',
+          },
+          {
+            pattern: 'assets/**',
+            group: 'sibling',
+            position: 'after',
+          },
+        ],
+        'newlines-between': 'always',
+      },
+    ],
+    // typescript eslint
+    '@typescript-eslint/no-unused-expressions': 0,
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'variable',
+        format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
+        leadingUnderscore: 'allow',
+      },
+    ],
+    '@typescript-eslint/no-use-before-define': 'off',
+  },
+  env: {
+    browser: true,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: [path.resolve(__dirname, 'src')],
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      },
+    },
+  },
+  ignorePatterns: ['vite.config.ts'],
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,48 @@
-import react from '@vitejs/plugin-react';
-import * as path from 'path';
-import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import react from "@vitejs/plugin-react";
+import * as path from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default () => {
   return defineConfig({
     plugins: [
-      react(),
+      process.env.npm_lifecycle_event === "build" ? null : react(),
       tsconfigPaths(),
       dts({
         insertTypesEntry: true,
       }),
     ],
     build: {
-      target: ['es2015'],
+      target: ["esnext"],
+      polyfillModulePreload: false,
       lib: {
-        entry: path.resolve(__dirname, 'src/index.ts'),
-        name: 'remember-ui',
+        entry: path.resolve(__dirname, "src/index.ts"),
+        name: "remember-ui",
         /** package.json과 파일 포멧을 맞춰야 한다
          * es -> package.json의 module과 동일 === dist/index.js
          * cjs -> package.json의 main 동일 === dist/index.cjm.js
          */
-        formats: ['es', 'cjs'],
+        formats: ["es", "cjs"],
         fileName: (format) => `index.${format}.js`,
       },
       rollupOptions: {
         external: [
-          '@tippy.js/react',
-          'react-toastify',
-          'react-outside-click-handler',
-          'react-text-mask',
-          'body-scroll-lock',
-          'react-modal',
+          "@tippy.js/react",
+          "react-toastify",
+          "react-outside-click-handler",
+          "react-text-mask",
+          "body-scroll-lock",
+          "react-modal",
+          "react",
+          "react-dom",
+          "styled-components",
         ],
         output: {
           globals: {
-            react: 'React',
-            'react-dom': 'ReactDOM',
-            'styled-components': 'styled',
+            react: "React",
+            "react-dom": "ReactDOM",
+            "styled-components": "styled",
           },
         },
       },
