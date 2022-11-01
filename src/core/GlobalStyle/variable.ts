92fab37669769313377d7c3ee898af9830df6f93
kimjeongwonn <kimjeongwonn@gmail.com>
2022-11-01 14:54:05 +0900

    PCWEB-8845 리브랜딩된 색상으로 변경 및 이전 네이밍 링크

--- src/core/GlobalStyle/variable.ts
+++ src/core/GlobalStyle/variable.ts
@@ -36,12 +36,11 @@ export const white = '#ffffff';
 export const text = gray400;
 export const borderColor = gray120;
 export const gray = gray200;
-// Primary 브랜드 컬러 표현 시 사용
-export const primary100 = '#212322';
-export const primary200 = '#ffffff';
 
-// Secondary 교체 예정으로 신규 화면에서 사용하지 않음
-export const secondary100 = '#e0a526';
+// Primary 브랜드 컬러 표현 시 사용
+export const primary100 = '#000000';
+export const primary200 = '#FFFFF9';
+export const secondary100 = '#FF5414';
 
 /**
  * colors in use
@@ -49,28 +48,50 @@ export const secondary100 = '#e0a526';
 
 // Contents  txt/ label/ border/ divider
 export const contents000 = '#212322';
-export const contents100 = '#646464';
-export const contents150 = '#8c8c8c';
-export const contents200 = '#aeaeae';
-export const contents300 = '#cccccc';
-export const contents999 = '#ffffff';
+export const contents100 = '#61615F';
+export const contents150 = '#8B8B87';
+export const contents200 = '#B2B1AD';
+export const contents300 = '#CDCCC6';
+export const contents999 = '#FFFFF9';
 
 // Disable disabled CTA
-export const disable = '#c7c7cd';
-export const disableAccent100 = '#ffbca6';
-export const disableAccent200 = '#a6c4ff';
+export const disabled = '#CDCCC6';
+export const disabledRoleRed = '#FFAA8D';
+export const disabledSecondary = '#FFCBBA';
+// legacy
+export const disable = disabled;
+export const disableAccent100 = disabledRoleRed;
+export const disableAccent200 = disabledSecondary;
 
 // Accent Error/ Link/
-export const accent100 = '#ea5128';
-export const accent200 = '#2864f0';
-export const background100 = '#ffffff';
-export const background200 = '#f5f5f5';
-export const background300 = '#ebebeb';
-export const backgroundAccent100 = '#faece8';
-export const backgroundAccent200 = '#eaf0fe';
-export const backgroundNew100 = '#fff9e5';
+export const roleRed = '#BB3811';
+export const roleYellow = '#EBA900';
+export const roleBlue = '#00B3EA';
+export const roleGreen = '#29B31E';
+// legacy
+export const accent100 = roleRed;
+export const accent200 = secondary100;
 
 // Background
+export const bg100 = '#FFFFFF';
+export const bg200 = '#F7F7F0';
+export const bg300 = '#E8E7E0';
+export const bgModal100 = '#FFFFFF';
+export const bgModal200 = '#F7F7F0';
+export const bgRoleRed = '#FCE9E6';
+export const bgSecondary100 = '#FFF2F0';
+export const bgHighlight = '#FCF6E0';
+export const bgRoleYellow = '#FCF6E0';
+export const bgRoleBlue = '#E0F5FC';
+export const bgRoleGreen = '#E7F6E5';
+// legacy
+export const background100 = bg100;
+export const background200 = bg200;
+export const background300 = bg300;
+export const backgroundAccent100 = bgRoleRed;
+export const backgroundAccent200 = bgSecondary100;
+export const backgroundNew100 = bgHighlight;
+
 export const mobileSizeBreak = '768px';
 export const mobileSmallSizeBreak = '320px';
 export const landingMobileSizeBreak = '1000px';
